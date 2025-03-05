04733a1d2483c2a7d7d3e878584eaf4c3a41eef8
HassanThePassionate <maqsoodia777@gmail.com>
2025-03-05 19:44:04 +0500

    Fixing the Some Errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
-
 import "./App.css";
 import AppName from "./Components/AppName";
 import AddTodo from "./Components/AddTodo";
 import TodoItems from "./Components/TodoItems";
 import WelcomeMessage from "./Components/WelcomeMessage";
 import { TodoItemContext } from "./Context/context";
+import { useReducer } from "react";
+
+const todoItemReducer = (state, action) => {
+  switch (action.type) {
+    case "NEW_ITEM":
+      return [
+        ...state,
+        { name: action.payload.itemName, dueDate: action.payload.itemDueDate },
+      ];
+    case "DELETE_ITEM":
+      return state.filter((item) => item.name !== action.payload.todoItemsName);
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [todoItems, dispatch] = useReducer(todoItemReducer, []);
+
+  const addNewItem = (itemName, itemDueDate) => {
+    dispatch({ type: "NEW_ITEM", payload: { itemName, itemDueDate } });
+  };
+
+  const deleteItem = (todoItemsName) => {
+    dispatch({ type: "DELETE_ITEM", payload: { todoItemsName } });
+  };
+
   return (
-    <TodoItemContext.Provider>
-      <center className="todo-container">
+    <TodoItemContext.Provider value={{ todoItems, addNewItem, deleteItem }}>
+      <center className='todo-container'>
         <div>
           <AppName />
           <AddTodo />
-          <WelcomeMessage></WelcomeMessage>
+          <WelcomeMessage />
           <TodoItems />
         </div>
       </center>
